53059204d48d801bada99285747ca649b36cca04
Elheyr <adria.peradalta@gmail.com>
2020-01-28 11:44:54 +0100

    dist/index.html

---
+++ dist/main.js
@@ -0,0 +1,21 @@
+const NAVSLIDE = () =>{
+    const burguer = document.querySelector('.burguer');
+    const nav = document.querySelector('nav ul');
+    const navLinks = document.querySelectorAll('nav ul li');
+
+    burguer.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ``;
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        burguer.classList.toggle(`toggle`);
+    });
+}
+
+NAVSLIDE();
