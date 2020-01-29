bf2ec0b9c2449b9e63fcedc9a4caf35134d6d710
Elheyr <adria.peradalta@gmail.com>
2020-01-29 14:00:46 +0100

    Updates

--- main.js
+++ main.js
@@ -19,3 +19,14 @@ const NAVSLIDE = () =>{
 }
 
 NAVSLIDE();
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-8vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
